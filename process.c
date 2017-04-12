9663c234ba782b75971502b42cb7372cdefb7cf8
Meghan Chen <mc2254@cornell.edu>
2017-04-12 14:35:04 -0700

    the uploaded versions

--- process.c
+++ process.c
@@ -1,107 +1,106 @@
-#include "shared_structs.h"
 #include "3140_concur.h"
-#include <stdlib.h>
 #include <fsl_device_registers.h>
+#include "shared_structs.h"
 
+/* the currently running process. current_process must be NULL if no process is running,
+    otherwise it must point to the process_t of the currently running process
+*/
 
-/* current process is NULL before scheduler starts and after it end */
-process_t * current_process = NULL;
-process_t * process_queue = NULL; // initialize queue's global variable keeping track of first process in queue
+process_t * current_process = NULL; 
+process_t * process_queue = NULL;
 
-void add_to_queue (process_t *new_process) 
-{
-	// list is empty, add to beginning
-	if (process_queue == NULL) {
-		process_queue = new_process;
-		new_process->next = NULL;
-	} 
-	// list is not empty, add to end
+static process_t * pop_front_process() {
+	if (!process_queue) return NULL;
+	process_t *proc = process_queue;
+	process_queue = proc->next;
+	proc->next = NULL;
+	return proc;
+}
+
+static void push_tail_process(process_t *proc) {
+	if (!process_queue) {
+		process_queue = proc;
+		proc->next = NULL;
+	}
 	else {
 		process_t *tmp = process_queue;
-		// traverse the list to the end
-		while (tmp->next != NULL) {
+		while (tmp->next) {
 			tmp = tmp->next;
 		}
-		// add new_process as the next of the current last
-		tmp->next = new_process;
-		new_process->next = NULL;
-		//tmp->next->next = NULL;
+		tmp->next = proc;
+		proc->next = NULL;
+	}
+	proc->next = NULL;
+}
+
+static void process_free(process_t *proc) {
+	process_stack_free(proc->orig_sp, proc->n);
+	free(proc);
+}
+
+
+/* Called by the runtime system to select another process.
+   "cursp" = the stack pointer for the currently running process
+*/
+unsigned int * process_select (unsigned int * cursp) {
+	if (cursp) {
+		if (current_process->waiting) {
+			current_process->sp = cursp;
+		}
+		else {
+			// Suspending a process which has not yet finished, save state and make it the tail
+			current_process->sp = cursp;
+			push_tail_process(current_process);
+		}
+	} 
+	else {
+		// Check if a process was running, free its resources if one just finished
+		if (current_process) {
+			process_free(current_process);
+		}
+	}
+	
+	// Select the new current process from the front of the queue
+	current_process = pop_front_process();
+	
+	if (current_process) {
+		// Launch the process which was just popped off the queue
+		return current_process->sp;
+	} else {
+		// No process was selected, exit the scheduler
+		return NULL;
 	}
 }
 
-/* Remove first element from queue and returns; we only ever remove first element of list
-When first element is only element, next is null. Therefore, the first element is null */
-process_t *remove_from_queue (void) {
-	if (process_queue == NULL) return NULL;
-	process_t *elem = process_queue;
-	process_queue = process_queue->next;
-	return elem;
-}
-
-/* Start process with the given amount of time
-*/
-void process_start (void)
-{
-	SIM->SCGC6 = SIM_SCGC6_PIT_MASK;
-	PIT_MCR &= 0;
-	PIT->CHANNEL[0].LDVAL = 0x1E8480; // load 2 million cycles (somewhere close to 10 Hz, >10 Hz)
-	PIT->CHANNEL[0].TFLG |= 1;
+/* Starts up the concurrent execution */
+void process_start (void) {
+	SIM->SCGC6 |= SIM_SCGC6_PIT_MASK;
+	PIT->MCR = 0;
+	PIT->CHANNEL[0].LDVAL = DEFAULT_SYSTEM_CLOCK / 100;
+	//PIT->CHANNEL[0].LDVAL = 0xFFFF;
 	NVIC_EnableIRQ(PIT0_IRQn);
+	// Don't enable the timer yet. The scheduler will do so itself
+	
+	// Bail out fast if no processes were ever created
+	if (!process_queue) return;
 	process_begin();
 }
 
-
-/* Creates a new process that starts at function f, initial stack size n 
-Returns 0 on success, -1 if error 
-May require allocating memory for a process_t structure, use malloc() 
-Should called process_stack_init() to allocate the stack 
-Should allocate a process_t 
-Should add the process to your scheduler's data structures */
-int process_create (void (*f) (void), int n)
-{
-	process_t *process = (process_t*) malloc(sizeof(process_t));
-	if (process == NULL) return -1;
-	unsigned int *new_sp = process_stack_init(f, n);
-	if (new_sp == NULL) return -1;
-	process->sp = new_sp;
-	//process->next = NULL;
-	//struct process_queue *new_process;
-	//new_process->val = process;
-	add_to_queue(process);
+/* Create a new process */
+int process_create (void (*f)(void), int n) {
+	unsigned int *sp = process_stack_init(f, n);
+	if (!sp) return -1;
+	
+	process_t *proc = (process_t*) malloc(sizeof(process_t));
+	if (!proc) {
+		process_stack_free(sp, n);
+		return -1;
+	}
+	
+	proc->sp = proc->orig_sp = sp;
+	proc->n = n;
+	proc->waiting = 0;
+	
+	push_tail_process(proc);
 	return 0;
-}
-
-
-/* updates process_t
-process_t is NULL until process_start is called
-process_t is also NULL when a process terminates
-current_process is process_t
-cursp is null if no process is running or when process just finished */
-unsigned int * process_select(unsigned int * cursp)
-{
-	if (cursp == NULL) {
-		if (process_queue == NULL) return NULL;
-		// remove first process in queue if it exists
-		// free the stack memory
-		// save process in a temp variable
-		//struct process_state *tmp = current_process;
-		// free the stack for the previous process and remove from the queue
-		process_t *tmp = current_process;
-		current_process = remove_from_queue();
-		process_stack_free(tmp->sp, sizeof(tmp));
-		free(tmp);
-		// update current_process; null if queue is empty
-		return current_process->sp;
-	}
-	// process was not done
-	else {
-		// kick current process to the back of the queue and remove from the beginning
-		current_process->sp = cursp;
-		// current_process->next = NULL;
-		add_to_queue(current_process);
-		current_process = remove_from_queue();
-		// the current process is now what is first in the queue
-		return current_process->sp;
-	}
-			
 }
